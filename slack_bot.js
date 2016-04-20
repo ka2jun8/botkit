1523980e8db882301f16b17635ddf4c2e916002a
kani <kani.junya@jp.fujitsu.com>
2016-04-20 17:02:47 +0900

    test commit.

--- slack_bot.js
+++ slack_bot.js
@@ -244,3 +244,113 @@ function formatUptime(uptime) {
     uptime = uptime + ' ' + unit;
     return uptime;
 }
+
+
+controller.hears(['ごはん'], 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.startConversation(message, function(err, convo) {
+        convo.ask('場所はどのへん？', [
+            {
+                pattern: '',
+                callback: function(response, convo) {
+                    console.log(response);
+                    console.log(convo);
+                    convo.ask('何系がいい？', [
+                        {
+                            pattern: '',
+                            callback: function(response, convo) {
+                                console.log(response);
+                                console.log(convo);
+                                convo.say('ふうん');
+                                convo.next();
+                            }
+                        }
+                    ]);
+                }
+            }
+        ]);
+    });
+/*
+            var json = req.body;
+
+            console.log("kani::: "+JSON.stringify(json));
+
+            // 受信テキスト
+            var search_place = json['result'][0]['content']['text'];
+            var search_place_array = search_place.split("\n");
+
+            //検索キーワード
+            var gnavi_keyword = "";
+            if(search_place_array.length == 2){
+                var keyword_array = search_place_array[1].split("、");
+                gnavi_keyword = keyword_array.join();
+            }
+
+            console.log("kani::: place="+search_place+"/key="+gnavi_keyword);
+
+            // ぐるなびAPI レストラン検索API
+            var gnavi_url = 'http://api.gnavi.co.jp/RestSearchAPI/20150630/';
+
+            console.log("gnavi = "+process.env.GNAVI_KEY);
+
+            // ぐるなび リクエストパラメータの設定
+            var gnavi_query = {
+                "keyid": process.env.GNAVI_KEY,
+                "format": "json",
+                "address": search_place_array[0],
+                "hit_per_page": 1,
+                "freeword": gnavi_keyword,
+                "freeword_condition": 2
+            };
+            var gnavi_options = {
+                url: gnavi_url,
+                headers : {'Content-Type' : 'application/json; charset=UTF-8'},
+                qs: gnavi_query,
+                json: true
+            };
+
+            // 検索結果をオブジェクト化
+            var search_result = {};
+
+            request.get(gnavi_options, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    if('error' in body){
+                        console.log("検索エラー" + JSON.stringify(body));
+                        return;
+                    }
+
+                    // 店名
+                    if('name' in body.rest){
+                        search_result['name'] = body.rest.name;
+                    }
+                    // 画像
+                    if('image_url' in body.rest){
+                        search_result['shop_image1'] = body.rest.image_url.shop_image1;
+                    }
+                    // 住所
+                    if('address' in body.rest){
+                        search_result['address'] = body.rest.address;
+                    }
+                    // 緯度
+                    if('latitude' in body.rest){
+                        search_result['latitude'] = body.rest.latitude;
+                    }
+                    // 軽度
+                    if('longitude' in body.rest){
+                        search_result['longitude'] = body.rest.longitude;
+                    }
+                    // 営業時間
+                    if('opentime' in body.rest){
+                        search_result['opentime'] = body.rest.opentime;
+                    }
+
+                    console.log("kani::: "+JSON.stringify(search_result));
+
+                    callback(null, json, search_result);
+
+                } else {
+                    console.log('error: '+ response.statusCode);
+                }
+            });
+    */
+
+});
